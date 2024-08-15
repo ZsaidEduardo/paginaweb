7cc0d80d0d69409db1d339237aea0f71acb69f20
manukito2012 <emanuelguanuco17@gmail.com>
2024-08-15 14:19:21 -0300

    se agrego los scrip faltantes en el componente about
    scrip de idiomas

--- frontend/src/app/component/about/about.component.ts
+++ frontend/src/app/component/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CargarScriptService } from '../../services/cargar-script.service';
 
 @Component({
   selector: 'app-about',
@@ -8,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+
+  constructor(  private _cargarScript: CargarScriptService) {
+
+    _cargarScript.carga(["pantallaDeCarga"]);
+    _cargarScript.carga(["header"]);
+    _cargarScript.carga(["idioma_banderas"]);
+    _cargarScript.carga(["configuraciones_idiomas/idioma_header"]);
+    _cargarScript.carga(["configuraciones_idiomas/idioma_nosotros"]);
+    _cargarScript.carga(["configuraciones_idiomas/idioma_footer"]);
+  }
 
 }
