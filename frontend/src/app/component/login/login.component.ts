38f159db352786bd5929dce37bf5ef765f3ce3f1
manukito2012 <emanuelguanuco17@gmail.com>
2024-08-15 16:17:48 -0300

    correcion en login

--- frontend/src/app/component/login/login.component.ts
+++ frontend/src/app/component/login/login.component.ts
@@ -1,20 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CargarScriptService } from '../../services/cargar-script.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  isVisible: boolean = false;
 
+  
 
   constructor(  private _cargarScript: CargarScriptService) {
 
     _cargarScript.carga(["configuraciones_idiomas/idioma_login"]);
     _cargarScript.carga(["altura_login"]);
+  }
+
+  ngOnInit() {
+    // La animación comienza cuando el componente se inicializa
+    this.isVisible = true;
   }
 
 
